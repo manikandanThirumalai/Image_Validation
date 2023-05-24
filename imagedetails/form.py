431902fc2889e877ed3c1f542f53c23a7fbf3ab2
imran-ideas2it <imran@ideas2it.com>
2023-05-24 11:05:44 +0530

    form update

--- imagedetails/form.py
+++ imagedetails/form.py
@@ -8,20 +8,21 @@ class ImageForm(forms.ModelForm):
         model = Image
         fields = ('title','description','category','image')
 
+    def clean_image(self):  
+        image_file=self.cleaned_data.get('image')
+        self.config = Validationdetails().validation_setting()
+        if not image_file:
+              raise ValidationError(self.config.image_empty_error)
+        else:
+             extension = image_file.name.split(".")[-1]
+        if extension not in self.config.image_format:
+               raise ValidationError(self.config.image_format_error)
+        if image_file:
+            if image_file.size > self.config.image_size:
+                raise ValidationError(self.config.image_size_error)
+            return image_file
+        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # call the validation_setting function
-        self.config = Validationdetails().validation_setting()
-        image = self.cleaned_data.get('image')
-
-        if not image:
-            raise ValidationError(self.config.image_empty_error)
-        else:
-            if image:
-                extension = image.name.split(".")[-1]
-            if extension not in self.config.image_format:
-                raise ValidationError(self.config.image_format_error)
-            if image.size > self.config.image_size:
-                raise ValidationError(self.config.image_size_error)
-            return image
-
+        self.fields['image'].error_messages['invalid_image'] = 'Please upload a JPG or PNG image.'
